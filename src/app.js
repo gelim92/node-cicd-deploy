26eff9500781356981c3906a7866502b40a5b8d6
Gerard <gerard.lim92@gmail.com>
2022-04-09 19:10:46 +0800

    feat: add lint

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => {
-	res.json({ data: 'Hello World updated!!' });
+  res.json({data: 'Hello World updated!!'});
 });
 
 const PORT = 4000;
 
 app.listen(PORT, () => {
-	console.log(`Listening to port ${PORT}`);
+  console.log(`Listening to port ${PORT}`);
 });
